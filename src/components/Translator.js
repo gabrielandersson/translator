35da08f60e83e22ea11c5a9511335669754f9b92
Kitt Cheung <kitt.cheung@live.se>
2022-01-28 13:34:26 +0100

    feat:(btn) handle text on btnClick

--- src/components/Translator.js
+++ src/components/Translator.js
@@ -5,9 +5,15 @@ import { useState } from "react";
 function Translator() {
 
 const [text, setText] = useState("")
+const [isClick, setClick] = useState(false)
 
-
-    
+const handleOnChange=(event)=>{
+setText(event.target.value)
+setClick(false)
+}
+const onBtnClick=() =>{
+setClick(true)
+}    
 
     return (
         <div className="Translator">
@@ -17,7 +23,7 @@ const [text, setText] = useState("")
                     <legend>What do you want to translate? </legend>
                     <div>
                         <br></br>
-                        <input class="TransInput" type="text" value={text} placeholder="Text to translate"></input>
+                        <input class="TransInput" type="text" value={text} onChange={handleOnChange} placeholder="Text to translate" ></input>
                         <br></br>
                         <br></br>
                         <button id="btn" >Translate</button>
@@ -25,7 +31,7 @@ const [text, setText] = useState("")
                         <br></br>
                         <br></br>
                         <br></br>
-                        <input class="TransInput" type="text" placeholder="Image Output"></input>
+                        <input class="TransInput" type="text" onClick={onBtnClick} placeholder="Image Output"></input>
                     </div>
                 </fieldset>
             </div>
